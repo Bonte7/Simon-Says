d8f527badc34b76a9bc322db6c7592eba0d1aba7
Christian Bonte <cjbonte7@gmail.com>
2021-08-20 08:30:44 -0400

    Added player functionality, animations for clicks, and audio for clicks.

--- game.js
+++ game.js
@@ -1,25 +1,69 @@
 //store the available colors in the game
 const buttonColors = ["red", "blue", "green", "yellow"];
 
+//store user selected colors
+let userClickedPattern = [];
+
 //array to store the color pattern for the ganme
 let gamePattern = [];
 
-
+/*--------- Game ---------- */
 //Start a pattern sequence on keypress
-$(document).keypress(function nextSequence() {
-  //generate a random num between 0 - 3
-  let randomNum = Math.floor(Math.random() * 4);
+$(document).keypress(function nextSequence(event) {
 
-  //grab a random color from the array and store it
-  let randomChosenColor = buttonColors[randomNum];
+//only proceed if the a key is pressed
+  if (event.key === 'a') {
+    //generate a random num between 0 - 3
+    let randomNum = Math.floor(Math.random() * 4);
 
-  //push the random color to the pattern array
-  gamePattern.push(randomChosenColor);
+    //grab a random color from the array and store it
+    let randomChosenColor = buttonColors[randomNum];
 
-  //get the id for the html element and animate a flash
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    //push the random color to the pattern array
+    gamePattern.push(randomChosenColor);
 
-  let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-  audio.play();
+    //get the id for the html element and animate a flash
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+  }
+  else {
+    console.log("Incorrect key pressed");
+  }
 
 });
+
+
+/*--------- Player ---------- */
+$(".btn").click(function(event) {
+
+  //get the id for the color that was clicked
+  let userChosenColor = event.currentTarget.id;
+
+  //push the color that was clicked to the user array
+  userClickedPattern.push(userChosenColor);
+
+  console.log(userClickedPattern);
+
+  //animate the clicks
+  animatePress(userChosenColor)
+  //$("#" + userChosenColor).fadeOut(100).fadeIn(100);
+
+  playSound(userChosenColor);
+});
+
+/*--------- Game Sounds ---------- */
+function playSound(name) {
+
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+
+}
+
+//// TODO: Fix the animation function
+/*--------- Animation ---------- */
+function animatePress(currentColor) {
+  setTimeout(function() {
+    $("#" + currentColor).addClass(".pressed");
+  }, 100);
+}
